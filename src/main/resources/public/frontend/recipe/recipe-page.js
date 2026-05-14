f0098270c7505fea1ad7d5d18b9f757f8219e075
JoshE-Git <JoshE-Git@github.com>
2026-05-14 22:43:22 +0000

    Test run: 114/128 passed, 14 failed, 0 skipped - 2026-05-14T22:43:22.294Z

--- src/main/resources/public/frontend/recipe/recipe-page.js
+++ src/main/resources/public/frontend/recipe/recipe-page.js
@@ -9,6 +9,31 @@ let recipes = [];
 // Wait for DOM to fully load before accessing elements
 window.addEventListener("DOMContentLoaded", () => {
 
-    
+    /* 
+     * TODO: Get references to various DOM elements
+     * - Recipe name and instructions fields (add, update, delete)
+     * - Recipe list container
+     * - Admin link and logout button
+     * - Search input
+    */
+   let adminLink = document.getElementById("admin-link");
 
+   let recipeList = document.getElementById("recipe-list");
+
+   let logoutButton = document.getElementById("logout-button");
+   let searchButton = document.getElementById("search-button");
+   let addRecipeButton = document.getElementById("add-recipe-submit-input");
+   let updateRecipeButton = document.getElementById("update-recipe-submit-input");
+   let deleteRecipeButton = document.getElementById("delete-recipe-submit-input");
+
+   let searchInput = document.getElementById("search-input");
+   let addRecipeInput = document.getElementById("add-recipe-name-input");
+   let updateRecipeInput = document.getElementById("update-recipe-name-input");
+   let deleteRecipeInput = document.getElementById("delete-recipe-name-input");
+
+   let addInstructionInput = document.getElementById("add-recipe-instructions-input");
+   let updateInstructionInput = document.getElementById("update-recipe-instructions-input");
+
+
+   
 });
